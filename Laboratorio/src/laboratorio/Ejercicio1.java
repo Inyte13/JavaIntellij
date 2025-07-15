be08e9a1f7337f617a19caeb7fd1b6813e6a9395
Inyte13 <luisgardaz13@gmail.com>
2025-07-15 17:13:48 -0500

    Actualización

--- Laboratorio/src/laboratorio/Ejercicio1.java
+++ Laboratorio/src/laboratorio/Ejercicio1.java
@@ -4,6 +4,27 @@
 //Tiempo: 2:00
 package laboratorio;
 
+import java.util.*;
+
 public class Ejercicio1 {
+	// Método amp
+	public static String amp (String adn, int ini, int fin){
+		String primer=adn.substring(0,ini);
+		String segundo=adn.substring(ini)
+		return 
+	}
+	public static String del (String adn, int ini, int fin){}
+	public static String inv(String adn, int ini, int fin){}
+	public static void main(String[] args) {
+		Scanner teclado=new Scanner(System.in);
+		System.out.println("Ingrese una cadena de ADN: ");
+		String adn=teclado.nextLine();
+		System.out.println("Cantidad de mutaciones: ");
+		int n=teclado.nextInt();
+		for(int i=0;i<n;i++) {
+			System.out.println("Ingrese la mutación "+n);
+			
+		}
+	}
 
 }
