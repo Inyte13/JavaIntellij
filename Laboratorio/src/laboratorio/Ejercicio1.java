627dc5feafb6967e3b06b4d742d693c9b2283d8d
Inyte13 <luisgardaz13@gmail.com>
2025-07-16 18:10:02 -0500

    Actualización

--- Laboratorio/src/laboratorio/Ejercicio1.java
+++ Laboratorio/src/laboratorio/Ejercicio1.java
@@ -7,36 +7,44 @@ package laboratorio;
 import java.util.*;
 
 public class Ejercicio1 {
-	// Método amp
-	public static String amp (String adn, int ini, int fin){
-		String primer=adn.substring(0,fin+1);
-		String segundo=adn.substring(fin+1,adn.length());
-		String duplicar=adn.substring(ini,fin+1);
-		return primer+duplicar+segundo;}
-	
-	public static String del (String adn, int ini, int fin){
-		String primer=adn.substring(0,ini);
-		String segundo=adn.substring(fin+1,adn.length());
-		return primer+segundo;}
-	
-	public static String inv(String adn, int ini, int fin){
-		String primer=adn.substring(0,ini);
-		String segundo=adn.substring(ini,fin);
-		String tercer=adn.substring(fin, adn.length());
-		String reverse=new StringBuilder(segundo).reverse().toString();
-		return primer+reverse+tercer;
-	}
-	public static void main(String[] args) {
-		Scanner teclado=new Scanner(System.in);
-		System.out.print("Ingrese una cadena de ADN: ");
-		String adn=teclado.nextLine();
-		/*System.out.print("Cantidad de mutaciones: ");
-		int n=teclado.nextInt();*/
-		System.out.println(inv(adn,1,5));
-		//System.out.println(del(adn,6,9));
-		//System.out.println(inv(adn,2,3));
-		/*for(int i=0;i<n;i++) {
-			System.out.println("Ingrese la mutación "+n);}*/
-	}
+    public static void main(String [] args) {
+        Scanner teclado=new Scanner(System.in);
+        System.out.print("Ingrese un ADN: ");
+        String cadena=teclado.next();
+        convertirAArray(cadena);
+        }
+    
+    public static ArrayList<String> convertirAArray(String cadena){ // Método convertirAArray
+    	ArrayList<String> array=new ArrayList<>(); // Inicializamos nuestro array
+    	for(int i=0;i<cadena.length();i++) { // Bucle del tamaño de la cadena
+    		array.add(cadena.substring(i,i+1));} // Sacamos cada caracter y lo añadimos al array
+    	return array;} 
+    public static String convertirAString(ArrayList<String> array){ // Método convertirAString
+    	String cadena=""; // Inicializamos cadena
+    	for(int i=0;i<array.size();i++) { // Bucle tamaño del array
+    		cadena+=array.get(i);} // Agrega cada valor del array a la cadena
+    	return cadena;}
+    public static ArrayList<String> amp(ArrayList<String> adn,int ini,int fin){ // Método amp
+    	ArrayList<String> nArray=new ArrayList<>(adn); // Copiamos el adn 
+    	ArrayList<String> duplicado=new ArrayList<>(); // Inicializamos duplicado
+    	for(int i=ini;i<fin;i++) { // Bucle del tamaño que se desea
+    		duplicado.add(nArray.get(i));} // Agregamos cada cáracter
+    	nArray.addAll(ini,duplicado); // Añadimos a la copia
+    	return nArray;}
+    public static ArrayList<String> del(ArrayList<String> adn,int ini,int fin){
+    	ArrayList<String> nArray=new ArrayList<>();
+    	for(int i=0;i<ini;i++) {
+    		nArray.add(adn.get(i));}
+    	for(int j=fin;j<adn.size();j++) {
+    		nArray.add(adn.get(j));}
+    	return nArray;}
+    public static ArrayList<String> inv(ArrayList<String> adn,int ini,int fin){
+    	
+    	
+    }
+    
+    
 
 }
+
+
