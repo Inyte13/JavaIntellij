424849180e1cff989aa1d5c5cfa0a05d493d6de2
Luis García <luisgardaz13@gmail.com>
2025-08-24 14:12:25 -0500

    Actualización

--- IDS/src/ids/Modificable.java
+++ IDS/src/ids/Modificable.java
@@ -5,24 +5,43 @@ import java.util.Scanner;
 public class Modificable {
 	public static void main(String[] args) {
 		Scanner teclado=new Scanner(System.in);
-		int matriz[][]= {{120,15,30},{65,1,2},{4,52,7},{46,32,16},{64,23,11},{61,12,91}};
-		String key=" ";
-		while(!key.equalsIgnoreCase("finish")) {
-			System.out.print("Ingrese el destino: ");
-			int f=teclado.nextInt();
-			System.out.print("Ingrese el horario: ");
-			int c=teclado.nextInt();
-			System.out.print("Ingrse la cantidad de pasajes que desea comprar: ");
-			int num=teclado.nextInt();
-
-			if(matriz[f][c]>=num) {
-				System.out.println("Su reseva fue realizada con éxito");
-				matriz[f][c]-=num;
-			}else {
-				System.out.println("No hay asientos disponibles");
+		char matriz[][]=new char[10][10];
+		boolean bandera=true;
+		boolean confirmacion=false;
+		for(int j=0;j<matriz.length;j++) {
+			for(int i=0;i<matriz[0].length;i++) {
+				matriz[j][i]='L';
 			}
-			System.out.print("¿Desea finalizar?: ");
-			key=teclado.next();
-		}	
+		}
+		while(bandera) {
+			System.out.print("¿Desea ver los asientos disponibles?: ");
+			confirmacion=teclado.nextBoolean();
+			if(confirmacion) {
+				mostrarMatriz(matriz);
+			}
+			System.out.print("Ingrese la fila: ");
+			int f=teclado.nextInt()-1;
+			System.out.print("Ingrese la columna: ");
+			int c=teclado.nextInt()-1;
+			if(matriz[f][c]=='L') {
+				System.out.println("¡Gracias por su compra!");
+				matriz[f][c]='X';
+			}else {
+				System.out.println("Este asiento esta ocupado");
+			}
+			System.out.print("Desea comprar de nuevo?: ");
+			bandera=teclado.nextBoolean();
+		}
+		
+	}
+	public static void mostrarMatriz(char matriz[][]) {
+		for(int j=0;j<matriz.length;j++) {
+			System.out.print(j+1+"\t");
+			for(int i=0;i<matriz[0].length;i++) {
+				System.out.print(matriz[j][i]+"\t");
+			}
+			System.out.println();
+		}				
+		
 	}
 }
