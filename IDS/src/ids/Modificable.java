75cb88d01ef4c4b073ecd9898a4bdd828b73ad0c
Luis García <luisgardaz13@gmail.com>
2025-08-24 10:31:09 -0500

    Actualización

--- IDS/src/ids/Modificable.java
+++ IDS/src/ids/Modificable.java
@@ -5,17 +5,24 @@ import java.util.Scanner;
 public class Modificable {
 	public static void main(String[] args) {
 		Scanner teclado=new Scanner(System.in);
-		System.out.print("Ingrese las filas: ");
-		int f=teclado.nextInt();
-		for(int c=0;c<f;c++) {
-			for(int emp=0;emp<=f-c-1;emp++) {
-				System.out.print(" ");
-			}
-			for(int ast=0;ast<c*2+1;ast++) {
-				System.out.print("*");
-			}			
-			System.out.println();
-		}
-	}
+		int matriz[][]= {{120,15,30},{65,1,2},{4,52,7},{46,32,16},{64,23,11},{61,12,91}};
+		String key=" ";
+		while(!key.equalsIgnoreCase("finish")) {
+			System.out.print("Ingrese el destino: ");
+			int f=teclado.nextInt();
+			System.out.print("Ingrese el horario: ");
+			int c=teclado.nextInt();
+			System.out.print("Ingrse la cantidad de pasajes que desea comprar: ");
+			int num=teclado.nextInt();
 
+			if(matriz[f][c]>=num) {
+				System.out.println("Su reseva fue realizada con éxito");
+				matriz[f][c]-=num;
+			}else {
+				System.out.println("No hay asientos disponibles");
+			}
+			System.out.print("¿Desea finalizar?: ");
+			key=teclado.next();
+		}	
+	}
 }
