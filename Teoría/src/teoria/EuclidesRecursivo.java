c6b12202fc1ddcf9c46c30b4e9e0cd0f2dade02d
Inyte13 <luisgardaz13@gmail.com>
2025-09-04 15:20:18 -0500

    Actualización

--- Teoría/src/teoria/EuclidesRecursivo.java
+++ Teoría/src/teoria/EuclidesRecursivo.java
@@ -1,16 +1,18 @@
-package ids;
+package teoria;
 
 import java.util.Scanner;
 
 public class EuclidesRecursivo {
 	
     public static int mcd(int a, int b) { // Método mcd
-        if(b==0){
-            return a;} // Caso base
-        else{
-            return mcd(b, a%b);}} // Usamos el propio Método mcd
+        if(b==0){ // Caso base
+            return a;
+        }else{
+            return mcd(b, a%b); // Usamos el propio Método mcd
+        }
+    } 
 
-    public static void main(String[] args) { // Main
+    public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         System.out.print("Ingrese el primer número: ");
         int num1 = teclado.nextInt();
@@ -18,4 +20,6 @@ public class EuclidesRecursivo {
         int num2 = teclado.nextInt();
         int mcd= mcd(Math.max(num1, num2), Math.min(num1, num2));
         System.out.println("El MCD es: " + mcd);
-        teclado.close();}}
+        teclado.close();
+    }
+}
