65ceeae7ae95ea168d26d1648dc1fecb0117696c
Luis García <luisgardaz13@gmail.com>
2025-09-05 21:31:26 -0500

    Actualización

--- Teoría/src/teoria/_String_.java
+++ Teoría/src/teoria/_String_.java
@@ -2,10 +2,12 @@ package teoria;
 
 public class _String_ {
 	public static void main(String[] args) {
-		String palabra="Hola";
-		char c=palabra.charAt(3); // Devuelve el caracter de la posición 3 (a)
-		String str=palabra.substring(0,2); // Devuelve un substring desde [0,2> (Ho)
-		int posicion=palabra.indexOf("i"); // Devuelve la posición en la que encontro "a" sino devuelve -1 (3) 
-		int size=palabra.length(); // Devuelve el tamaño del string (4)
-		System.out.println(palabra.matches("Hola")); // Verífica si cumple el patrón (true)
+		String str="Hola";
+		str.charAt(3); // Devuelve el caracter de la posición 3 (a)
+		str.substring(0,2); // Devuelve un substring desde [0,2> (Ho)
+		str.indexOf("i"); // Devuelve la posición en la que encontro "a" sino devuelve -1 (3) 
+		str.length(); // Devuelve el tamaño del string (4)
+		str.replace("a", ""); // Remplaza todas las "a" y las cambia por ""
+		str.toLowerCase(); // Minúsculas
+		System.out.println(str); // Verífica si cumple el patrón (true)
 }}
