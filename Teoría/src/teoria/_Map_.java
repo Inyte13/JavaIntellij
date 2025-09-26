79c6da3fa304162416f06ddc9acb2b01cd3d7c15
Luis García <luisgardaz13@gmail.com>
2025-09-26 13:09:40 -0500

    Actualización

--- Teoría/src/teoria/_Map_.java
+++ Teoría/src/teoria/_Map_.java
@@ -4,17 +4,19 @@ import java.util.*;
 
 public class _Map_ {
 	public static void main(String[] args) {
-        Map<String, Integer> edades=new HashMap<>();
-        edades.put("Luis", 18); // (key, value)
-        edades.put("Ana", 17); // (key, value)
-        edades.put("Pedro", 74); // (key, value)
-        System.out.println(edades.get("Ana")); // Saca el valor del Map (17)
-        System.out.println(edades.containsKey("Luis")); // Tiene la key Luis? (true)
-        System.out.println(edades.containsValue(18)); // Tiene el value 18? (true)
-        System.out.println(edades.isEmpty()); // Está vacío? (false)
-        for (Map.Entry<String, Integer> entrada : edades.entrySet()) {
-            System.out.println(entrada.getKey()+" - "+entrada.getValue());}
-        edades.remove("Pedro");
-        System.out.println(edades); 
-        for(Map.Entry<String, Integer> e:edades.entrySet()) { // Recorrer el Map
-        	System.out.println(e.getKey()+" - "+e.getValue());}}}
+    HashMap<Integer,String> alumnos=new HashMap<>();
+    alumnos.put(1,"Mafer");
+    alumnos.put(2,"Luis");
+    alumnos.put(3,"Lucia");
+    alumnos.get(1); // Devuleve el value de la key 1
+    alumnos.containsKey(5); // Existe la key 5?
+    alumnos.containsValue("Aitana"); // Existe el value Aitana?
+    alumnos.isEmpty(); // El HashMap está vacío?
+    for(Map.Entry<Integer,String> e: alumnos.entrySet()){
+      System.out.println(e.getKey()+"-"+e.getValue());
+    }
+    alumnos.remove(3); // Al eliminar, solo puede ir la key
+    alumnos.remove(2,"Luis"); // Solo elimina si existe una key 2 con el value "Mafer", si es así devuelve true
+    System.out.println(alumnos);
+  }
+}
