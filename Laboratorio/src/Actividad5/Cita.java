b833f35e0fbddfb75519da59654b2563cb21a453
Luis García <luisgardaz13@gmail.com>
2025-10-16 02:08:19 -0500

    Actualización

---
+++ Laboratorio/src/Actividad5/Cita.java
@@ -0,0 +1,76 @@
+package Actividad5;
+
+public class Cita {
+  private int codigoCita;
+  private Paciente paciente;
+  private Doctor doctor;
+  private String fecha;
+  private String hora;
+  private String estado;
+
+  public Cita(int codigoCita, Paciente paciente, Doctor doctor, String fecha, String hora, String estado) {
+    this.codigoCita = codigoCita;
+    this.paciente = paciente;
+    this.doctor = doctor;
+    this.fecha = fecha;
+    this.hora = hora;
+    this.estado = estado;
+  }
+
+  public int getCodigoCita() {
+    return codigoCita;
+  }
+
+  public void setCodigoCita(int codigoCita) {
+    this.codigoCita = codigoCita;
+  }
+
+  public Paciente getPaciente() {
+    return paciente;
+  }
+
+  public void setPaciente(Paciente paciente) {
+    this.paciente = paciente;
+  }
+
+  public Doctor getDoctor() {
+    return doctor;
+  }
+
+  public void setDoctor(Doctor doctor) {
+    this.doctor = doctor;
+  }
+
+  public String getFecha() {
+    return fecha;
+  }
+
+  public void setFecha(String fecha) {
+    this.fecha = fecha;
+  }
+
+  public String getHora() {
+    return hora;
+  }
+
+  public void setHora(String hora) {
+    this.hora = hora;
+  }
+
+  public String getEstado() {
+    return estado;
+  }
+
+  public void setEstado(String estado) {
+    this.estado = estado;
+  }
+
+  public void mostrarInfo() {
+    System.out.println("Código de cita: " + codigoCita);
+    System.out.println("Paciente: " + paciente.getNombre());
+    System.out.println("Doctor: " + doctor.getNombre());
+    System.out.println("Fecha: " + fecha);
+    System.out.println("Hora: " + hora);
+    System.out.println("Estado: " + estado);
+  }
+}
